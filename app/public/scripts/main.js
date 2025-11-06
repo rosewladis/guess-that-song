a554649cddafa43fddc9272000d14d790aae7cd1
rachellepena <rmp356@drexel.edu>
2025-11-06 16:44:08 -0500

    comment out console logs to clean up logs

--- app/public/scripts/main.js
+++ app/public/scripts/main.js
@@ -3,10 +3,10 @@ const mouseClickSound = document.getElementById('sound');
 document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('mousedown', (event) => {
         if (event.button == 0 && (event.target.nodeName == 'BUTTON' || event.target.classList.contains('abutton'))) {
-            console.log('Play click sound for', event.target.id);
+            // console.log('Play click sound for', event.target.id);
             setTimeout(() => {
                 mouseClickSound.play();
-                console.log('Wait for audio');
+                // console.log('Wait for audio');
             }, 1);
         }
     });
